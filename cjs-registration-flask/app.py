52362778be7a43f4bd4f598eea89c388516f0ec0
ebubeokoye <okoyeebube02@gmail.com>
2025-01-03 23:33:12 +0000

    hello-flask

--- cjs-registration-flask/app.py
+++ cjs-registration-flask/app.py
@@ -2,11 +2,22 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app= Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/DOB", methods=["GET", "POST"])
+@app.route("/DOB", methods=["POST"])
 def DOB():
-    if request.method=="POST":
+    email=request.form.get("email")
+    if not email:
+        return "Failed"
+    else:
         return render_template("DOB.html")
+
+@app.route("/success", methods=["POST"])
+def success():
+    dob=request.form.get("dob")
+    if not dob:
+        return "failure"
+    else:
+        return render_template("success.html")
