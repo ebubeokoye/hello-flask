f7fdbabacc14e5d27d076163573c0ca4ebfd49dc
ebubeokoye <okoyeebube02@gmail.com>
2025-01-03 23:35:49 +0000

    hello-flask

--- signup-database-flask/app.py
+++ signup-database-flask/app.py
@@ -1,18 +1,71 @@
 from flask import Flask, redirect, render_template, request
+import sqlite3
+
+
 app=Flask(__name__)
 
+GENDERS=["Female", "Male"]
 #lets connect the database to RAM
-import sqlite3
-connection=sqlite3.connect("signup.db")
 
 # create a cursor() method to be able to execute sql queries using python
-db=connection.cursor()
 
-app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    if request.method=="GET":
-        return render_template("index.html")
+    return render_template("index.html")
 
-app.route("register", methods=["GET", "POST"])
-def register():
-    return render_template("register.html")
+@app.route("/signuppage1")
+def signuppage1():
+    return render_template("signuppage1.html", genders=GENDERS)
+
+@app.route("/validatepage1", methods=["POST"])
+def validatepage1():
+
+    connection=sqlite3.connect("signup.db")
+    db=connection.cursor()
+
+    email=request.form.get("email")
+    password=request.form.get("password")
+    firstname=request.form.get("firstname")
+    lastname=request.form.get("lastname")
+    
+    if not email:
+        return redirect("/")
+    if not password:
+        return redirect("/")
+    if not firstname:
+        return redirect("/")
+    if not lastname:
+        return redirect("/")
+    if request.form.get("gender") not in GENDERS:
+        return redirect("/")
+    
+    data=db.execute("SELECT email FROM signup WHERE email='" + email +"'")
+    datalist=data.fetchall()
+    for item in datalist:
+        if email in item:
+            return redirect("/login")
+        else:
+            return redirect("/registered")
+    return redirect("/registered")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/validatelogin", methods=["POST"])
+def validatelogin():
+    connection=sqlite3.connect("signup.db")
+    db=connection.cursor()
+
+    email=request.form.get("email")
+    password=request.form.get("password")
+    if not email:
+        return redirect("/login")
+    if not password:
+        return redirect("/login")
+
+    return render_template("registered.html")
+
+@app.route("/registered")
+def registered():
+    return render_template("registered.html")
--- signup-database-flask/signup.sql
+++ signup-database-flask/signup.sql
@@ -2,6 +2,6 @@
 -- email TEXT NOT NULL, password INTEGER NOT NULL
 -- );
 
--- INSERT INTO signup(id, email, password) VALUES (2, 'chic@co.com', 'uery');
+-- INSERT INTO signup(email, password) VALUES ('london@co.com', 'ottawa');
 
 SELECT * FROM signup;
