6019dfb56d254bec18b560ac8fda1c0122c98b18
ebubeokoye <okoyeebube02@gmail.com>
2025-01-28 14:24:35 +0000

    hello-flask

--- netflix-project-flask/app.py
+++ netflix-project-flask/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, redirect, render_template, request
 
 app =Flask (__name__)
+TEXT = ["Remember me"]
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", text=TEXT)
