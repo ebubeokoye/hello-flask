74d1a74cb9d58bad3dcc1a39c6e5646d9d0d0c74
ebubeokoye <okoyeebube02@gmail.com>
2025-04-01 22:01:25 +0100

    hello-flask

--- h_and_p-flask/app.py
+++ h_and_p-flask/app.py
@@ -22,26 +22,42 @@ def createaccountuser():
 def createaccountvendor():
     return render_template("createaccount-vendor.html")
 
-@app.route("/createaccount-agent")
-def createaccountagent():
-    return render_template("createaccount-agent.html")
+@app.route("/createaccount-land-agent")
+def createaccountlandtagent():
+    return render_template("createaccount-land-agent.html")
+
+@app.route("/createaccount-housing-agent")
+def createaccounthousingagent():
+    return render_template("createaccount-housing-agent.html")
 
 # when the user clicks on Buy from the landing page
 @app.route("/buylinkfrompage2", methods=["POST"])
 def buylinkfrompage2():
     return render_template("buylinkfrompage2.html")
 
-# this section upto number.... is for the user after they've registered/logged in
+# this section is for the user after they've registered/logged in
 @app.route("/homepage", methods=["GET", "POST"])
 def homepage(): 
     if request.method == "POST":
         return render_template("homepage.html")
     else: 
         return render_template("homepage.html")
-
+    
+# user buy land listing
 @app.route("/buyland")
 def buyland(): 
     return render_template("user-buyland.html")
+
+@app.route("/user_oneacre_buyland_listingpg2")
+def user_oneacre_buyland_listingpg2():
+    return render_template("user-oneacre-buyland-listingpg2.html")
+
+@app.route("/user_viewdetails_general_form")
+def user_viewdetails_general_form():
+    return render_template("user-viewdetails-general-form.html")
+
+
+
 
 @app.route("/rent")
 def rent(): 
@@ -54,3 +70,86 @@ def houseitems():
 @app.route("/user-account")
 def useraccount(): 
     return render_template("user-account.html")
+
+# this section is for the land agent
+
+@app.route("/land_agent_homepage", methods=["GET", "POST"])
+def land_agent_homepage():
+    if request.method == "POST":
+        return render_template("landagent-homepage.html")
+    else:
+        return render_template("landagent-homepage.html")
+    
+@app.route("/agent_land_listing", methods=["GET", "POST"])
+def agent_land_listing():
+    if request.method == "POST":
+        return redirect("/agent_land_listing")
+    else:
+        return render_template("agent-land-listing.html")
+        
+@app.route("/landing_agent_oneacre_listingpg2", methods=["GET", "POST"])
+def landing_agent_oneacre_listingpg2():
+    if request.method == "POST":
+        return render_template("landing-agent-oneacre-listingpg2.html")
+    else:
+        return render_template("landing-agent-oneacre-listingpg2.html")
+
+# this section is for the housing agent
+
+@app.route("/housing_agent_homepage", methods=["GET", "POST"])
+def housing_agent_homepage():
+    if request.method =="POST":
+        return render_template("housing-agent-homepage.html")
+    else:
+        return render_template("housing-agent-homepage.html")
+# items listing
+@app.route("/agent_housing_listing", methods=["GET", "POST"])
+def agent_housing_listing():
+    if request.method == "POST":
+        return render_template("agent-housing-listing.html")
+    else:
+        return render_template("agent-housing-listing.html")
+
+@app.route("/housing_agent_bungalow_listingpg2",  methods=["GET", "POST"])
+def housing_agent_bungalow_listingpg2():
+    if request.method == "POST":
+        return render_template("housing-agent-bungalow-listingpg2.html")
+    else:
+        return render_template("housing-agent-bungalow-listingpg2.html")
+
+
+@app.route("/form_to_add_item_housingagent")
+def form_to_add_item_housingagent():
+    return render_template("form-to-add-item-housingagent.html")
+
+@app.route("/form_to_edit_item_housingagent")
+def form_to_edit_item_housingagent():
+    return render_template("edit-item-houseagent.html")
+
+# Insights section
+@app.route("/insights_page_housingagent")
+def insights_page_housingagent():
+    return render_template("insightspage-housingagent.html")
+
+# this section is for the vendor after they've registered/logged in
+@app.route("/vendor_homepage", methods=["GET", "POST"])
+def vendor_homepage():
+    if request.method == "POST":
+        return render_template("vendor-homepage.html")
+    else:
+        return render_template("vendor-homepage.html")
+    
+@app.route("/vendor_listing", methods=["GET", "POST"])
+def vendor_listing():
+    if request.method == "POST":
+        return render_template("vendor-listing.html")
+    else:
+        return render_template("vendor-listing.html")
+    
+@app.route("/vendor_kitchenitems_listingpg2", methods=["GET", "POST"])
+def vendor_kitchenitems_listingpg2():
+    if request.method == "POST":
+        return render_template("vendor-kitchenitems-listingpg2.html")
+    else:
+        return render_template("vendor-kitchenitems-listingpg2.html")
+    
